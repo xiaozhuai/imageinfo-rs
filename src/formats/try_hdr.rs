d228a1567c584dc3fe6345c346e912c314294bcc
xiaozhuai <798047000@qq.com>
2024-07-03 13:13:45 +0800

    Optimize hdr

--- src/formats/try_hdr.rs
+++ src/formats/try_hdr.rs
@@ -1,5 +1,4 @@
 use crate::{ImageFormat, ImageInfo, ImageInfoError, ImageInfoResult, ImageSize, ReadInterface};
-use regex::Regex;
 use std::cmp::min;
 use std::io::{BufRead, Seek};
 use std::str::FromStr;
@@ -12,13 +11,11 @@ where
     if length < 6 {
         return Err(ImageInfoError::UnrecognizedFormat);
     }
-    let mut offset = 6usize;
     let buffer = ri.read(0, 6)?;
     if !buffer.cmp_any_of(0, 6, vec![b"#?RGBE", b"#?XYZE"]) {
         if length < 10 {
             return Err(ImageInfoError::UnrecognizedFormat);
         }
-        offset = 10usize;
         let buffer = ri.read(0, 10)?;
         if !buffer.cmp(0, 10, b"#?RADIANCE") {
             return Err(ImageInfoError::UnrecognizedFormat);
@@ -27,32 +24,43 @@ where
 
     let piece = 64usize;
     let mut header = String::new();
-    let x_pattern = Regex::new(r"\s[-+]X\s(\d+)\s").unwrap();
-    let y_pattern = Regex::new(r"\s[-+]Y\s(\d+)\s").unwrap();
+    let mut resolution_start = 0usize;
+    let mut resolution = "";
+    let mut offset = 0usize;
     while offset < length {
         let buffer = ri.read(offset, min(length - offset, piece))?;
         offset += buffer.len();
         header += &(buffer.to_string());
-        let x_captures = x_pattern.captures(&header);
-        let y_captures = y_pattern.captures(&header);
-        if let (Some(x_captures), Some(y_captures)) = (x_captures, y_captures) {
-            if x_captures.len() < 2 || y_captures.len() < 2 {
-                return Err(ImageInfoError::UnrecognizedFormat);
+        if resolution_start == 0 {
+            if let Some(pos) = header.find("\n\n") {
+                resolution_start = pos + 2;
+            } else {
+                continue;
             }
-            let width = &x_captures[1];
-            let height = &y_captures[1];
-            return Ok(ImageInfo {
-                format: ImageFormat::HDR,
-                ext: "hdr",
-                full_ext: "hdr",
-                mimetype: "image/vnd.radiance",
-                size: ImageSize {
-                    width: i64::from_str(width).unwrap(),
-                    height: i64::from_str(height).unwrap(),
-                },
-                entry_sizes: vec![],
-            });
+        }
+        if let Some(pos) = &header[resolution_start..].find('\n') {
+            resolution = &header[resolution_start..resolution_start + pos];
+            break;
         }
     }
-    Err(ImageInfoError::UnrecognizedFormat)
+    if resolution.is_empty() {
+        return Err(ImageInfoError::UnrecognizedFormat);
+    }
+    let tokens: Vec<&str> = resolution.split(' ').collect();
+    if tokens.len() != 4 {
+        return Err(ImageInfoError::UnrecognizedFormat);
+    }
+    let y_str = tokens[1];
+    let x_str = tokens[3];
+    Ok(ImageInfo {
+        format: ImageFormat::HDR,
+        ext: "hdr",
+        full_ext: "hdr",
+        mimetype: "image/vnd.radiance",
+        size: ImageSize {
+            width: i64::from_str(x_str).unwrap(),
+            height: i64::from_str(y_str).unwrap(),
+        },
+        entry_sizes: vec![],
+    })
 }
